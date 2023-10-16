2abbf0bb1568405a4b18974a99bd283ccdf38f89
Silvia <silviatc1993@gmail.com>
2023-10-16 08:51:22 +0200

    initial commit

---
+++ src/components/App.jsx
@@ -0,0 +1,24 @@
+//imports dependencias, images, componentes, styles
+// import { useEffect, useState } from 'react';
+// import callToApi from '../services/api';
+// import ls from '../services/localStorage';
+import '../styles/App.scss';
+
+function App() {
+  //states
+
+  //effects
+
+  //handlers from events
+
+  //renders
+
+  //html
+  return (
+    <>
+      <p></p>
+    </>
+  );
+}
+
+export default App;
